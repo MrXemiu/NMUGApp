750b554c2795f53ec27f4bafa409d4b22711e761
Gabe Chavez <gabefc@gmail.com>
2017-10-20 08:24:52 -0600

    add Mvx app to core project

---
+++ NMUGApp.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Core;
+using NMUGApp.Core.Services;
+using NMUGApp.Core.ViewModels;
+
+namespace NMUGApp.Core
+{
+    public class App : MvxApplication
+    {
+        public App()
+        {
+            Mvx.RegisterType<IHttpClientService, HttpClientService>();
+            Mvx.RegisterType<IPressReleaseService, PressReleaseService>();
+
+            RegisterNavigationServiceAppStart<LoginViewModel>();
+        }
+    }
+}
