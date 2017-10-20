8cfbb0583d24f0b293968deacefd1dcb0772c8eb
Gabe Chavez <gabefc@gmail.com>
2017-10-20 06:45:29 -0600

    add viewmodel stubs

---
+++ NMUGApp.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.Core.ViewModels;
+
+namespace NMUGApp.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+    }
+}
