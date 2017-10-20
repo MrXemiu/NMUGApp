6f861fdef0b45e571f84503ff61cf800e0d31610
Gabe Chavez <gabefc@gmail.com>
2017-10-20 08:25:21 -0600

    add setup and Mvx Android.Support.Design dependencies

---
+++ NMUGApp/Setup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Android.Content;
+using Android.Support.Design.Widget;
+using MvvmCross.Binding.Bindings.Target.Construction;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Droid.Views;
+using NMUGApp.Core;
+
+namespace NMUGApp
+{
+    public class Setup : MvxAppCompatSetup
+    {
+        public Setup(Context applicationContext) : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+
+        /// <summary>
+        /// Fill the Binding Factory Registry with bindings from the support library.
+        /// </summary>
+        protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
+        {
+            MvxAppCompatSetupHelper.FillTargetFactories(registry);
+            base.FillTargetFactories(registry);
+        }
+
+        /// <summary>
+        /// This is very important to override. The default view presenter does not know how to show fragments!
+        /// </summary>
+        protected override IMvxAndroidViewPresenter CreateViewPresenter()
+        {
+            return new MvxAppCompatViewPresenter(AndroidViewAssemblies);
+        }
+
+        protected override IEnumerable<Assembly> AndroidViewAssemblies => new List<Assembly>(base.AndroidViewAssemblies)
+        {
+            typeof(TextInputLayout).Assembly,
+            typeof(TextInputEditText).Assembly
+        };
+    }
+}
